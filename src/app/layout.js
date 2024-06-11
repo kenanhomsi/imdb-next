7029866a73ce9da5e41968959354ddfb0e861e1e
kenanhomsi <kinanhomsi0959@gmail.com>
2024-06-11 16:06:35 +0300

    add navbar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import {Inter} from 'next/font/google'
 import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 import Providers from "./Providers";
 
 export const metadata = {
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Providers>
             <Header />
+            <Navbar />
             {children}
         </Providers>
       </body>
