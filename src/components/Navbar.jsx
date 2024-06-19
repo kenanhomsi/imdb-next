356c4c3b584805e98fb109459cb76b0fb12cfe83
kenanhomsi <kinanhomsi0959@gmail.com>
2024-06-19 18:19:40 +0300

    fix error 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import NavbarItem from "./NavbarItem";
+import {Suspense} from "react";
 
 export default function Navbar() {
   return (
     <div className="flex dark:bg-gray-600  bg-amber-100 p-4 lg:text-lg justify-center gap-6">
-        <NavbarItem  title='Trending' param='fetchTrending'/>
-        <NavbarItem title='Top Rated' param="fetchTopRated" />
+        <Suspense fallback={<>Loading...</>}>
+            <NavbarItem  title='Trending' param='fetchTrending'/>
+            <NavbarItem title='Top Rated' param="fetchTopRated" />
+        </Suspense>
+        
     </div>
   )
 }
